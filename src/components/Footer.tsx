963c24d7f65b18e717b0134b2de0c2938ea9de7e
Saeb Masarwa <saeb2312@gmail.com>
2024-12-26 06:27:01 +0200

    Add: version in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,49 +6,52 @@ export default function Footer() {
   const { user } = useContext(UserContext);
 
   return (
-    <div className="mt-5">
-      <figure className="figure">
-        <i className="bi bi-info-circle"></i>
-        <figcaption className="figure-caption">
-          <NavLink
-            className="text-decoration-none"
-            aria-current="page"
-            to="/about"
-          >
-            About
-          </NavLink>
-        </figcaption>
-      </figure>
-      {user !== null && (
-        <>
-          <figure className="figure ms-3">
-            <i className="bi bi-heart-fill"></i>
-            <figcaption className="figure-caption">
-              <NavLink
-                className="text-decoration-none"
-                aria-current="page"
-                to="/favcards"
-              >
-                Favorites
-              </NavLink>
-            </figcaption>
-          </figure>
-        </>
-      )}
-      {user?.isBusiness && (
-        <figure className="figure ms-3">
-          <i className="bi bi-card-list"></i>
+    <>
+      <div className="mt-5">
+        <figure className="figure">
+          <i className="bi bi-info-circle"></i>
           <figcaption className="figure-caption">
             <NavLink
               className="text-decoration-none"
               aria-current="page"
-              to="/mycards"
+              to="/about"
             >
-              My Cards
+              About
             </NavLink>
           </figcaption>
         </figure>
-      )}
-    </div>
+        {user !== null && (
+          <>
+            <figure className="figure ms-3">
+              <i className="bi bi-heart-fill"></i>
+              <figcaption className="figure-caption">
+                <NavLink
+                  className="text-decoration-none"
+                  aria-current="page"
+                  to="/favcards"
+                >
+                  Favorites
+                </NavLink>
+              </figcaption>
+            </figure>
+          </>
+        )}
+        {user?.isBusiness && (
+          <figure className="figure ms-3">
+            <i className="bi bi-card-list"></i>
+            <figcaption className="figure-caption">
+              <NavLink
+                className="text-decoration-none"
+                aria-current="page"
+                to="/mycards"
+              >
+                My Cards
+              </NavLink>
+            </figcaption>
+          </figure>
+        )}
+      </div>
+      <div>version: 1.0.10</div>
+    </>
   );
 }
